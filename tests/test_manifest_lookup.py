e0085a94b8d153abd06e454cf6f4f8401865629c
sghosal <sghosal@chanzuckerberg.com>
2025-11-04 16:11:07 -0800

    Remove all logs from notebooks

--- tests/test_manifest_lookup.py
+++ tests/test_manifest_lookup.py
@@ -13,7 +13,7 @@ from utils.assets import CreSequencesManifestLookup, GeneSequencesManifestLookup
 
 import logging
 logging.basicConfig(
-   level=logging.DEBUG,
+   level=logging.INFO,
    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
    datefmt='%Y-%m-%d %H:%M:%S'
    )
--- utils/assets.py
+++ utils/assets.py
@@ -14,7 +14,7 @@ import duckdb
 
 import logging
 logging.basicConfig(
-   level=logging.DEBUG,
+   level=logging.INFO,
    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
    datefmt='%Y-%m-%d %H:%M:%S'
    )
